0da443f3c216e8d1a6325098efce198abd5b7bc6
YuryXXl <y7183605555@icloud.com>
2025-03-16 15:07:33 -0600

    Python-Package-1-done

---
+++ 14_precedence_puzzle/solution.py
@@ -0,0 +1,5 @@
+result_with_parentheses = (10 + 5) * (8 - 3) / (2 ** 2)
+print("Result with parentheses:", result_with_parentheses)
+
+result_without_parentheses = 10 + 5 * 8 - 3 / 2 ** 2
+print("Result without parentheses:", result_without_parentheses)
---
+++ 15_modulus_mystery/solution.py
@@ -0,0 +1,5 @@
+result_floor_div = -10 // 3
+print("-10 // 3 =", result_floor_div)
+
+result_modulus = -10 % 3
+print("-10 % 3 =", result_modulus)

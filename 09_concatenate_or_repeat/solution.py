aa457fd1ca4696ae5e38f32191011c0531c2170f
YuryXXl <y7183605555@icloud.com>
2025-03-16 14:37:49 -0600

    second-part

---
+++ 09_concatenate_or_repeat/solution.py
@@ -0,0 +1,9 @@
+first_name = "Alice"
+last_name = "Doe"
+
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
+
+repeated_word = "Python " * 5
+print("Repeated:", repeated_word)   
+
--- 11_randomizer_challenge/solution.py
+++ 11_randomizer_challenge/solution.py
@@ -1,1 +1,9 @@
+import random
+print(random.random())
+print(random. randint(1, 50))
+color = ["red", "blue", "green", "black", "yellow"]
+print(random.choice(color))
+fruits = ["apple", "banana", "plum", "orange"]
+random.shuffle(fruits)
+print(fruits)
 
